4f8eff461e2fe47b93af7b0da756c04e2182c562
NiklasGanske <ganske25@web.de>
2025-03-17 13:41:14 +0100

    /hotfix circualr dependencies

--- web_shop_backend/order_service/src/main/java/order_service/order_service/config/SecurityConfig.java
+++ web_shop_backend/order_service/src/main/java/order_service/order_service/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableWebSecurity
@@ -33,8 +32,4 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 }
